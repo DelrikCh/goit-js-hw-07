f31cb2c4b7fb4ada6247f8f11f6a0903c481d963
Roman Choporov <ChoporovRoman@gmail.com>
2024-06-26 11:10:41 -0700

    Complete

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const categoriesList = document.querySelector('ul#categories');
+const categories = categoriesList.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+  const title = category.querySelector('h2').textContent;
+  const items = category.querySelectorAll('li').length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${items}`);
+});
